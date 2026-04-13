04839543253c34e104aec20e866b90585cf504ab
kk7jji <kk7jji@outlook.com>
2026-04-13 16:45:55 -0700

    linting bfs execute method.

--- lib/knights_travails/bfs.rb
+++ lib/knights_travails/bfs.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
+# project namespace
 module KnightsTravails
+  # breadth first search algorithm
   class BFS
     attr_accessor :visited, :queue, :start, :goal
 
@@ -12,8 +14,7 @@ module KnightsTravails
     end
 
     def execute
-      node = Node.new(parent: nil, state: start)
-      queue.push(value: node)
+      init_queue
 
       until queue.empty?
         node = queue.pop
@@ -21,12 +22,20 @@ module KnightsTravails
         return node if node.state == goal
 
         node.state.next_states.each do |next_state|
-          next_node = Node.new(parent: node, state: next_state)
-          queue.push(value: next_node) unless visited.visited?(next_node)
+          push_next_node(parent: node, state: next_state)
         end
       end
 
       nil
+    end
+
+    def init_queue
+      queue.push(value: Node.new(parent: nil, state: start))
+    end
+
+    def push_next_node(parent: nil, state: nil)
+      next_node = Node.new(parent: parent, state: state)
+      queue.push(value: next_node) unless visited.visited?(next_node)
     end
   end
 end
